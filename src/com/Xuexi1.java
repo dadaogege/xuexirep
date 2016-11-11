eb511d321bedaae93b3de9e87ea22b57c4d08e73
liuxin <liuxin@ubuntu>
2016-11-11 00:23:59 -0800

    hh

--- src/com/Xuexi1.java
+++ src/com/Xuexi1.java
@@ -1,5 +1,4 @@
 package com;
-
 import java.util.HashMap;
 
 public class Xuexi1{
@@ -13,6 +12,7 @@ public class Xuexi1{
 	   thread3.start();
 	   
 	   //System.exit(-1);
+///////////////////////////////////////////////////////////////////////////////
    }
    public static class RunableWork1 implements Runnable{
        public HashMap map=null;
